7b371cf70712c1278bad030c981673c2046e194e
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-30 18:56:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carpma_uygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 10; a++)
+            {
+                for (int b = 1; b < 10; b++)
+                {
+                    Console.Write("{0}*{1}={2}      ", b, a, (b * a));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
